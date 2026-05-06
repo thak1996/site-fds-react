528987aacb4f14254a1b8ec294d0ab5195e6da5b
Franklyn Viana dos Santos <franklyn_vs_@hotmail.com>
2026-05-06 19:32:09 -0300

    refactor: update Quote entity and validation schema to use nullable and optional fields for phone, observations, elevator, and floor

--- src/core/entities/Quote.ts
+++ src/core/entities/Quote.ts
@@ -11,11 +11,11 @@ export interface QuoteAddress extends Address {
 export interface Quote {
   type: 'residencial' | 'comercial';
   name: string;
-  residential_phone: string;
-  commercial_phone: string;
+  residential_phone?: string | null;
+  commercial_phone?: string | null;
   mobile_phone: string;
   email: string;
   origin: QuoteAddress;
   destination: QuoteAddress;
-  observations: string;
+  observations?: string | null;
 }
